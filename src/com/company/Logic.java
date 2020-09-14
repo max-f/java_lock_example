073d0b115c973523de41ebb9019abff867a78c60
Max Fischer <max.fischer@diva-e.com>
2020-09-14 14:54:41 +0200

    Add async test

--- src/com/company/Logic.java
+++ src/com/company/Logic.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Logic {
 
     private static final int RECURSIVE_CALLS = 3;
@@ -11,13 +15,14 @@ public class Logic {
     public void prepare(final DataStruct ds) {
         ds.getLock().lock();
         try {
-            post(RECURSIVE_CALLS, ds);
+//            postSync(RECURSIVE_CALLS, ds);
+            postAsync(RECURSIVE_CALLS, ds);
         } finally {
             ds.getLock().unlock();
         }
     }
 
-    public void post(final int count, final DataStruct ds) {
+    public void postSync(final int count, final DataStruct ds) {
         if (count == 0) {
             return;
         }
@@ -26,9 +31,41 @@ public class Logic {
         final int holdCount = ds.getLock().getHoldCount();
         final int queueLength = ds.getLock().getQueueLength();
 
+        waitForMe();
         System.out.println(String.format("Thread name: %s - Lock hold count: %d - Lock queue length: %d", threadName,
-                holdCount, queueLength));
+                                         holdCount, queueLength));
 
-        post(count - 1, ds);
+        postSync(count - 1, ds);
     }
+
+
+    public void postAsync(final int count, final DataStruct ds) {
+        if (count == 0) {
+            return;
+        }
+
+        final String threadName = Thread.currentThread().getName();
+        final int holdCount = ds.getLock().getHoldCount();
+        final int queueLength = ds.getLock().getQueueLength();
+
+//        final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> waitForMe());
+
+        System.out.println(String.format("Thread name: %s - Lock hold count: %d - Lock queue length: %d", threadName,
+                                         holdCount, queueLength));
+
+        postAsync(count - 1, ds);
+    }
+
+
+    public String waitForMe() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            //NOP
+        }
+        return "test";
+    }
+
 }
